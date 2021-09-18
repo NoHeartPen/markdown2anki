99966ba6585a42c1a79f00a5699012457a437ba0
NoHeartPen <79316356+NoHeartPen@users.noreply.github.com>
2021-09-18 19:33:42 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,49 +1,33 @@
 import os
+from re import I
 def replace_formula(formula):
-    formula_str = formula.group()
-    formula_str = formula_str.replace('$', '')
-    formula_str = formula_str.replace('{{', '{ {')
+    formula_str = formula.group() # str.group()
+    formula_str = formula_str.replace('$', '') 
+    formula_str = formula_str.replace('{{', '{ {') 
     formula_str = formula_str.replace('}}', '} }')
     formula_str = '\(' + formula_str + '\)'
     return formula_str
 
 def replace_cloze(cloze):
     cloze_str = cloze.group()
-    cloze_str = cloze_str.replace('**', '')
-    cloze_str = '{{c1:' + cloze_str + '}}'
+    cloze_str = cloze_str.replace('**', '') 
+    cloze_str = '{{c1::' + cloze_str + '}}' 
     return cloze_str
+
+def replace_picture(picture): #将markdown的图片语法转换为Anki支持的html
+    picture_str = picture.group()
+    picture_str = picture_str.replace('![](','')
+    picture_str = picture_str.replace(')','')
+    picture_str = '<img src=' + picture_str + '>'
+    return picture_str
+
+def replace_code(code): #将md格式的代码语法转换为Anki支持的html语法
+    code_str = code.group()
+    code_str = code_str.replace('`','')
+    code_str = '<code>' + code_str + '<\code>'
+    return code_str
 
 def replace_Bold(Bold):
     Bold_str = Bold.group()
     Bold_str = "<b>"+Bold_str.replace('**', '')+"</b>"
     return Bold_str
-
-def replace_img(img):
-    img_str= img.group()
-    img_str =img_str.replace("![","")
-    img_str =img_str.replace("]","")
-    if os.path.exists(AnkiMedia()+img_str):#判断图片在Anki用户的媒体文件夹内是否存在
-        img_str = "<img src="+img_str.split("\\")[-1]+">"
-    # else:
-    #     img_str=""
-    return img_str
-
-def AnkiMedia(name="SoneSton"):#请输入Anki资源用户名
-    # mediaPath="%APPDATA%\\Anki2"+name+"\\collection.media"
-    return os.getenv('APPDATA')+"\\Anki2"+"\\"+name+"\\collection.media\\" #返回的文件夹路径后面带 \
-
-def Anki_Saveimg(ImgPath,name="SoneSton"):
-    if os.path.exists(ImgPath)==False: #判断图片本身是否存在
-        print(ImgPath.split("\\")[-1]+"写入"+name+"的Anki媒体库失败,原因:图片文件不存在")
-        return False
-    AnkiPath=AnkiMedia(name)
-    if os.path.exists(AnkiPath+ImgPath.split("\\")[-1])==True:#判断图片在Anki用户的媒体文件夹内是否存在
-        pass
-    else:
-        import shutil
-        shutil.copy(ImgPath,AnkiMedia(name))
-        print(ImgPath.split("\\")[-1]+"写入"+name+"的Anki媒体库成功")
-    return True
-
-def dirname(filePath):#返回文件的目录
-    return os.path.dirname(filePath)+"\\"
